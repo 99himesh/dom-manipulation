90aa58b9e0ec271a562bfb9d721d5eb23e10e58c
himesh <himeshkashyap347@gmail.com>
2022-11-14 22:09:54 -0800

    task 9

--- demo7.js
+++ demo7.js
@@ -1,58 +1,45 @@
+
 var form=document.getElementById('addForm');
 var itemList=document.getElementById('items');
 
 
 form.addEventListener('submit',addItem);
-itemList.addEventListener('click',removeitem);
+
+
       function  addItem(e){
         e.preventDefault();
 
         //to get  value from imput
       var newItem=document.getElementById('item').value;
+     // add description value
+      var newItem1=document.getElementById('item1').value;
       //create a new li
       var li=document.createElement('li');
       //add class
       li.className='list-group-item';
       
-      //add text node with input value
-      li.appendChild(document.createTextNode(newItem));
+      //add text node with input value of item and description
+      li.appendChild(document.createTextNode(newItem+' '+newItem1));
 
 
-      // add button
 
-      var deletebtn=document.createElement('button');
 
-     //add class name
-     deletebtn.className='btn btn-danger btn-sm float-right delete';
+var filter=document.getElementById('filter');
+var itemList=document.getElementById('items');
 
-     //append textnode
+filter.addEventListener('keyup',filteritem);
 
-     deletebtn.appendChild(document.createTextNode('x'));
-     
-     // append button to li
-     li.appendChild(deletebtn);
-     //append li to list
-      itemList.appendChild(li);
-    
-
-    // add edit button 
-   var editbtn=document.createElement('button');
-  //add class name
-   editbtn.className='float-right mx-2';
-    //append text node
-   editbtn.appendChild(document.createTextNode('edit'));
-   //append button to li
-      li.appendChild(editbtn);
-   //append li to list
-    itemList.appendChild(li);
- }
-
-    //remove element
-    function removeitem(e){
-      if(e.target.classList.contains('delete')){
-        if(confirm('are you sure')){
-          var li=e.target.parentElement;
-          itemList.removeChild(li);
-        }
-      }
-    }
+function filteritem(e){
+//convert text to lowerCase 
+  var text=e.target.value.toLowerCase();
+  var item=itemList.getElementsByTagName('li');
+  Array.from(item).forEach(function(item){
+     var itemName =item.firstChild.textContent;
+     var desli=item.childNodes[1].textContent;
+     if(itemName.toLocaleLowerCase().indexOf(text) !=-1  || desli.toLowerCase().indexOf(text!=-1   )){
+        item.style.display='block';
+     }else{
+        item.style.display='none';
+     }
+  });
+}

396070e24d1018569841264123436ddbff24b717
himesh <himeshkashyap347@gmail.com>
2022-11-14 22:09:18 -0800

    task 9

--- demo6.js
+++ demo6.js
@@ -3,19 +3,22 @@ var itemList=document.getElementById('items');
 
 
 form.addEventListener('submit',addItem);
-itemList.addEventListener('click',removeitem);
+
+
       function  addItem(e){
         e.preventDefault();
 
         //to get  value from imput
       var newItem=document.getElementById('item').value;
+     // add description value
+      var newItem1=document.getElementById('item1').value;
       //create a new li
       var li=document.createElement('li');
       //add class
       li.className='list-group-item';
       
-      //add text node with input value
-      li.appendChild(document.createTextNode(newItem));
+      //add text node with input value of item and description
+      li.appendChild(document.createTextNode(newItem+' '+newItem1));
 
 
       // add button
@@ -47,6 +50,7 @@ itemList.addEventListener('click',removeitem);
     itemList.appendChild(li);
  }
 
+ itemList.addEventListener('click',removeitem);
     //remove element
     function removeitem(e){
       if(e.target.classList.contains('delete')){
@@ -56,3 +60,28 @@ itemList.addEventListener('click',removeitem);
         }
       }
     }
+
+
+
+
+
+
+
+   var filter=document.getElementById('filter');
+   var itemList=document.getElementById('items');
+
+filter.addEventListener('keyup',filteritem);
+
+function filteritem(e){
+//convert text to lowerCase 
+  var text=e.target.value.toLowerCase();
+  var item=itemList.getElementsByTagName('li');
+  Array.from(item).forEach(function(item){
+     var itemName =item.firstChild.textContent;
+     if(itemName.toLocaleLowerCase().indexOf(text) !=-1){
+        item.style.display='block';
+     }else{
+        item.style.display='none';
+     }
+  });
+}
